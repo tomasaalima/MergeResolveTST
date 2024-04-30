94ab17bff879022cff80a539ac72d7d0eeaa4502
tomasaalima <tomasabdias22@gmail.com>
2024-04-30 20:12:16 -0300

    right version

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 import { createServer } from 'node:http';
 
 const hostname = '127.0.0.1';
@@ -7,8 +6,12 @@ const port = 3000;
 const server = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('A request has been received'); // Change message
+  res.end('A request are received');
 });
+
+// Versão "right" adiciona um comentário diferente
+/* Este é um comentário na versão "right" */
+console.log('This is a right version.');
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
