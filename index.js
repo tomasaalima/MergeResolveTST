c7bb92347c04745f9c3f70d59d6a588f94ee883e
tomasaalima <tomasabdias22@gmail.com>
2024-04-30 09:55:03 -0300

    original response

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = 3000;
 const server = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end('Hello World'); // Original response
 });
 
 server.listen(port, hostname, () => {
